fcbcc3cba7707cbe2514860da85572672bdc58aa
Martin Leopold <m@martinleopold.com>
2020-11-05 18:12:44 +0100

    flatten data (LVAs)

--- app/data.js
+++ app/data.js
@@ -1,4 +1,4 @@
-const FILE_LVAS = '../data/lvas/Alle LVs WS2019 SS2020 mit lehrveranstaltung_id.csv';
+const FILE_EVENTS = '../data/lvas/Alle LVs WS2019 SS2020 mit lehrveranstaltung_id.csv';
 
 const FOLDER_STUDIEN = '../data/studien';
 const STUDIEN = ['227_311', '228', '254_392', '255_4', '256_312', '257_364', '258_184', '297', '298_377', '299', '300_41', '301', '302_309', '303', '327_345', '328', '329_351', '330_320', '331', '332_292', '334', '335_352', '336_286', '476_347', '578', '585', '692', '768_80', '779_321', '804_179', '854'];
@@ -35,12 +35,97 @@ async function load_csv(url) {
   return lines;
 }
 
-
-export async function load() {
-  let lvas = await load_csv(FILE_LVAS);
+// raw data
+export async function load_data() {
+  let events = await load_csv(FILE_EVENTS);
   let studien_promises = STUDIEN.map(id => {
     return load_json(`${FOLDER_STUDIEN}/${id}.json`);
   });
   let studien = await Promise.all(studien_promises);
-  return { lvas, studien };
+  return { events, studien };
+}
+
+let stats = {};
+
+// extract lvas from module
+function reduce_module(acc_lvas, module) {
+  module = Object.assign( {}, module );
+  
+  // track lehrtypen
+  if (module.lehrtyp in stats.lehrtypen) stats.lehrtypen[module.lehrtyp] += 1;
+  else stats.lehrtypen[module.lehrtyp] = 1;
+  
+  let lvas = module.childs; 
+  delete module.childs;
+  if (lvas) { // could be undefined (empty module)
+    // add module property to lva
+    lvas = lvas.map( lva => Object.assign(lva, {_modul:module}) );
+    
+    // track lehrtypen
+    for (const lva of lvas) {
+      if (lva.lehrtyp in stats.lehrtypen) stats.lehrtypen[lva.lehrtyp] += 1;
+      else stats.lehrtypen[lva.lehrtyp] = 1;
+    }
+    
+    // lva could be a nested module
+    let nested_modules = lvas.filter(lva => lva.lehrtyp === 'modul');
+    let nested_lvas = nested_modules.reduce(reduce_module, [])
+    stats.nested_modules += nested_modules.length;
+    
+    lvas = lvas.filter(lva => lva.lehrtyp !== 'modul');
+    acc_lvas = acc_lvas.concat( lvas, nested_lvas );
+  } else {
+    stats.empty_modules += 1;
+  }
+  return acc_lvas;
+}
+
+// extract modules from studium
+function reduce_studium(acc_modules, studium) {
+  studium = Object.assign( {}, studium.data, {_name: studium.name, _studiengang_kz: studium.studiengang_kz, _url: studium.url} );
+  let semester = studium.lehrveranstaltungen; // {1: [modules of 1st semester], 2: [modules of 2nd semester], ...}
+  delete studium.lehrveranstaltungen;
+  
+  semester = Object.entries(semester); // [ [1, modules of 1st sem], [2, modules of 2nd sem], ...]
+  let modules = semester.flatMap( ([sem_nr, modules]) => {
+    return modules.map(mod => Object.assign( {}, mod, {_sem_nr:sem_nr, _studium:studium} ));
+  });
+  acc_modules = acc_modules.concat( modules );
+  return acc_modules;
+}
+
+export async function load() {
+  let data = await load_data();
+  
+  stats = {
+    lehrtypen: {},
+    empty_modules: 0,
+    nested_modules: 0,
+    lvas_without_events: 0,
+  };
+  
+  // flatten dataset
+  let modules = data.studien.reduce(reduce_studium, []);
+  let lvas = modules.reduce(reduce_module, []);
+  
+  // calendar structure: {lehrveranstaltung_id: [event, ...], ...}
+  let calendar = data.events.reduce( (cal, x) => {
+    if (x.lehrveranstaltung_id in cal) cal[x.lehrveranstaltung_id].push(x);
+    else if (x.lehrveranstaltung_id) cal[x.lehrveranstaltung_id] = [x];
+    return cal;
+  }, {});
+  
+  // add calendar (veranstaltungsorte) to lvas
+  lvas = lvas.map(lva => {
+    let cal = [];
+    if (lva.lehrveranstaltung_id in calendar) cal = calendar[lva.lehrveranstaltung_id];
+    if (cal.length == 0) stats.lvas_without_events += 1;
+    return Object.assign(lva, {_calendar: cal});
+  });
+  
+  return {
+    raw: data,
+    lvas,
+    stats
+  };
 }
